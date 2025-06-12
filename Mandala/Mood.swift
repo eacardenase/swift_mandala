b8a0ef82a8deab9aea900b575db65f342954de2d
Edwin Cardenas <ecardenas@lwolf.com>
2025-06-12 18:51:29 -0500

    feat: initialize moods with generated asset symbols instead of custom extensions

--- Mandala/Mood.swift
+++ Mandala/Mood.swift
@@ -12,3 +12,15 @@ struct Mood {
     var image: UIImage
     var color: UIColor
 }
+
+extension Mood {
+    static let angry = Mood(name: "angry", image: .angry, color: .angryRed)  // built-in within Xcode
+    //    static let angry = Mood(name: "confused", image: UIImage(usingResource: .angry), color: .angry)  // via extension
+    static let confused = Mood(name: "confused", image: .confused, color: .confusedPurple)
+    static let crying = Mood(name: "crying", image: .crying, color: .cryingLightBlue)
+    static let goofy = Mood(name: "goofy", image: .goofy, color: .goofyOrange)
+    static let happy = Mood(name: "happy", image: .happy, color: .happyTurquoise)
+    static let meh = Mood(name: "meh", image: .meh, color: .mehGray)
+    static let sad = Mood(name: "sad", image: .sad, color: .sadBlue)
+    static let sleepy = Mood(name: "sleepy", image: .sleepy, color: .sleepyLightRed)
+}
